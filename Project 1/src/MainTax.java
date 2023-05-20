6b4213df7efa6b881c336af609c5bdd9fe750761
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-05-20 07:26:42 +0530

    Delete MainTax.java

--- Project 1/src/MainTax.java
+++
@@ -1,8 +0,0 @@
-public class MainTax {
-    public static void main(String[] args){
-        var calculator = new TaxCalculator2023(100_000);
-        var report = new TaxReport();
-        report.show(calculator);
-        report.show(new TaxCalculator2022());
-    }
-}
